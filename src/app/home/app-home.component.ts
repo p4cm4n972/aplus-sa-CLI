11f6094fab6c1edac59c5ba10689c4f0b6f4ddec
p4cm4n972 <manuel.adele@gmail.com>
2017-10-17 17:34:10 +0200

    ok

---
+++ src/app/home/app-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component ({
+    templateUrl: './app-home.component.html',
+    styleUrls: ['./app-home.component.css']
+})
+export class HomeComponent {
+    public pageTitle: string = 'Accueil';
+}
