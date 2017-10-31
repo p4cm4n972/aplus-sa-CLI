662e8f2e1a51491bda8e3b5135ae703f28305bef
p4cm4n972 <manuel.adele@gmail.com>
2017-10-31 10:35:37 +0100

    product HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/app-home.component';
@@ -21,6 +22,7 @@ import { ProductDetailComponent } from './product/product-detail.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: 'history', component: HistoryComponent},
